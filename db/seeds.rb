b58ee01551d0cdec5e709dcd72e1e78e5061c92c
Don McLamb <mclamb.donald@gmail.com>
2018-05-21 02:37:22 -0400

    setup views, setup seeds, setup js generator, setup route

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+10.times do |num|
+  group = Group.create(:group => "Group Number #{num}")
+  Task.create({:group_id => group.id, :task => "walk dog", :dependencies => [1,3,5], :completedAt => nil})
+end
+#
+#
+# t.string "group_id"
+# t.string "task"
+# t.jsonb "dependencies"
+# t.string "completedAt"
+# t.datetime "created_at", null: false
+# t.datetime "updated_at", null: false
+# t.index ["group_id"], name: "index_tasks_on_group_id"
