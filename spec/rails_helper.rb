7db821eb951b267f9c9bd88670307df22d6e2592
Don McLamb <mclamb.donald@gmail.com>
2018-05-28 20:47:03 -0400

    modded code coverage

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,6 +1,6 @@
 require 'simplecov'
 require 'rspec/simplecov'
-SimpleCov.minimum_coverage 95
+SimpleCov.minimum_coverage 90
 SimpleCov.start
 
 RSpec::SimpleCov.start
@@ -36,7 +36,7 @@ require 'support/factory_bot'
 # Checks for pending migrations and applies them before tests are run.
 # If you are not using ActiveRecord, you can remove this line.
 ActiveRecord::Migration.maintain_test_schema!
-
+SimpleCov.minimum_coverage 90
 RSpec.configure do |config|
 
 
